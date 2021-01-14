66ce47b44a88385f859df04235725a00c583eba9
Jakub Jafra <jakub.jafra@gmail.com>
2021-01-14 17:33:24 +0100

    Build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p ./build
+cp ./kcalto.php ./build
+cp -R ./src ./build
+
+INFOFILE="./releases/info.json"
+
+VERSION=$(jq -r '.version' $INFOFILE)
+echo "Building version '$VERSION'..."
+
+FILENAME=./releases/packages/kcalto-$VERSION.zip
+
+if test -f "$FILENAME"; then
+  echo "Version '$VERSION' already exists, aborting.\nUpdate 'version' string in './releases/info.json' to continue."
+  exit 1
+fi
+
+cp -R ./build ./kcalto
+zip -r $FILENAME ./kcalto
+rm -rf ./kcalto
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$(jq --arg v "$DATE" '.last_updated = $v' $INFOFILE)" > $INFOFILE
+echo "$(jq --arg v "packages/kcalto-$VERSION.zip" '.relative_download_url = $v' $INFOFILE)" > $INFOFILE
